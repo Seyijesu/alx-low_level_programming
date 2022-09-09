d9645ce9ca7d401f8120a7ae7bcadd82e933007b
Seyijesu <olalekeoluwaseyi@gmail.com>
2022-09-09 16:56:20 -0700

    3 commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - using puts function to output 
+ * @puts - this accept a string
+ * return 0
+ */
+int main(){
+	puts("Programming is like building a multilingual puzzle");
+	return 0;
+}
