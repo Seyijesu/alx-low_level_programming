1fb62b203d1861dc06fe18cab533af710547e69d
Seyijesu <olalekeoluwaseyi@gmail.com>
2022-09-09 18:44:10 -0700

    task 5 and 6 committed

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/** 
+ * main - using printf() function
+ *
+ * Return: main return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return 0;
+}
