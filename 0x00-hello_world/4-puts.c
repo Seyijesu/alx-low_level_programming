bb46266e32886383f54b78973a4973142766cd2f
Seyijesu <olalekeoluwaseyi@gmail.com>
2022-09-09 17:51:44 -0700

    correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - using puts function to output 
+ * main - Entry point
  *
- * @puts - this accept a string
- *
- * Return : 0
+ * Return : always 0
  */
-int main(void){
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
