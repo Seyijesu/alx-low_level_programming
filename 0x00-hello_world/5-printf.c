f624db79b38ccc2d7aaaa122d2ddfa9285478d17
Seyijesu <olalekeoluwaseyi@gmail.com>
2022-09-09 19:14:24 -0700

    correction of 5 and 6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /** 
- * main - using printf() function
+ * main - Entring point
  *
  * Return: main return 0
- */
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
-	return 0;
+	return (0);
 }
