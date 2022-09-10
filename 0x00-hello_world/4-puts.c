fb086c8e8fcdb502f758cffcc95d9ce90c7a1814
Seyijesu <olalekeoluwaseyi@gmail.com>
2022-09-09 17:15:09 -0700

    correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  * @puts - this accept a string
  * return 0
  */
-int main(){
-	puts("Programming is like building a multilingual puzzle\n");
+int main(void){
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
