dca4dd59d790547b1e99720425cbf392daef3e85
Olaleke Oluwaseyi <106757684+Seyijesu@users.noreply.github.com>
2022-09-10 01:43:12 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 /**
  * main - using puts function to output 
+ *
  * @puts - this accept a string
- * return 0
+ *
+ * Return : 0
  */
 int main(void){
 	puts("\"Programming is like building a multilingual puzzle\n");
