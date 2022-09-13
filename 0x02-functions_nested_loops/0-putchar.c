ada0ca42f3c5c941fbccda471285aea58c536a23
Seyijesu <olalekeoluwaseyi@gmail.com>
2022-09-13 16:40:15 -0700

    function in c commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <main.h>
+
+/**
+ * main - this is the wntry pont of the program
+ *
+ * Return: alaways return 0
+ *
+**/
+
+int main(void)
+{
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ * _putchar - thid function take a character and output it to the stdo it return int 
+ * 
+ * @c: this is the parameter a character
+ *
+ * Return: it return 0 or 1
+ */
+int _putchar(char c);
+#endif
