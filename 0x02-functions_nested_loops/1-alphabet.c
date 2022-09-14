1dc73ebed33a70272bf41a1c98440a697a7a17ac
Seyijesu <olalekeoluwaseyi@gmail.com>
2022-09-13 17:10:27 -0700

    print alphabet commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - this print the lower case of english alphabet
+ *
+ * Return: this return a void
+ *
+**/
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = a; i <= z; i++)
+	{
+		_putchar('i');
+
+	}
+
+	_putchar('\n');
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,4 +8,12 @@
  * Return: it return 0 or 1
  */
 int _putchar(char c);
+
+/**
+ * print_alphabet - to print all lower case of the english alphabet
+ *
+ * Return: void
+ *
+**/
+void print_alphabet(void);
 #endif
