c4d4a30cd86dd1b2f77b2f1ef919e1637eac99cb
Seyijesu <olalekeoluwaseyi@gmail.com>
2022-09-15 19:36:36 -0700

    is digit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -19,4 +19,13 @@ int _putchar(char c);
  */
 int _isupper(int c);
 
+
+/**
+ * _isdigit - this check if a character is an digit
+ *
+ * @c: parameter to compare
+ *
+ * Return: 1 for true and 0 for false
+ */
+int _isdigit(int c);
 #endif
