6d3c643365c8847d28d254a90bf8035edbafd1dd
Seyijesu <olalekeoluwaseyi@gmail.com>
2022-09-20 15:20:41 -0700

    return sataement removed

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,13 @@
 #include "main.h"
 
+/**
+ * reset_to_98 - this reset the value of int to 98
+ *
+ * @n: this is the pointer parameter
+ *
+ */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
-	return;
 }
