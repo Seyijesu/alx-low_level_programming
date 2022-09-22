0def5d66da855f9eaa259f45fc8859f487e6aa14
Seyijesu <olalekeoluwaseyi@gmail.com>
2022-09-21 18:18:31 -0700

    string concertenation

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,3 +6,15 @@
  * @src: source of array to be used
  */
 char *_strcat(char *dest, char *src);
+
+
+/**
+ * _strncat - concetename two array
+ *
+ * @dest: destinantion
+ * @src: source
+ * @n: number of character to concertename
+ *
+ * Return: concertenated dest
+ */
+char *_strncat(char *dest, char *src, int n);
