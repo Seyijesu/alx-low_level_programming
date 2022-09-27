43942e373184d79f45219db0efaa0ae817b66f14
Seyijesu <olalekeoluwaseyi@gmail.com>
2022-09-27 16:25:05 -0700

    _strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -16,7 +16,7 @@ unsigned int _strspn(char *s, char *accept)
 	int j;
 	unsigned int len = 0;
 
-	for (i = 0; s[i] != ','; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
