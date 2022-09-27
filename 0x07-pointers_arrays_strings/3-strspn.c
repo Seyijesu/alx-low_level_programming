39752005a2bd8218408be4237a2c157c79cc97fb
Seyijesu <olalekeoluwaseyi@gmail.com>
2022-09-27 15:25:19 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strspn - function that gets the length of a prefix substring
+ *
+ * @s: the main string to scan
+ *
+ * @accept: stings that we are looking for
+ *
+ * Return: number of how times those char appare in the 1 segement
+ *
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	unsigned int len = 0;
+
+	for (i = 0; s[i] != ',' || s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accpet[j])
+			{
+				len++;
+			}
+		}
+	}
+
+	return (len);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -28,3 +28,15 @@ char *_memcpy(char *dest, char *src, unsigned int n);
  * Return: s startting from the first occurrence of c to the end of s
  */
 char *_strchr(char *s, char c);
+
+/**
+ * _strspn - function that gets the length of a prefix substring
+ *
+ * @s: the main string to scan
+ *
+ * @accept: stings that we are looking for
+ *
+ * @Return: number of how times those character appare in the first segement
+ *
+ */
+unsigned int _strspn(char *s, char *accept);
