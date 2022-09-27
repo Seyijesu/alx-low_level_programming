2ecd175dd9130728aee500c0f9b1dc9af3279dcb
Seyijesu <olalekeoluwaseyi@gmail.com>
2022-09-27 11:35:51 -0700

    memcpy

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,3 +6,17 @@
  * Return: char value of s
  */
 char *_memset(char *s, char b, unsigned int n);
+
+/**
+ * _memcpy -  function that copies memory area
+ *
+ * @dest: pointer to the source of data to be copied
+ *
+ * @src: sourc to copy byte from
+ *
+ * @n: number of byte to copy
+ *
+ * Return: dest
+ *
+ */
+char *_memcpy(char *dest, char *src, unsigned int n);
