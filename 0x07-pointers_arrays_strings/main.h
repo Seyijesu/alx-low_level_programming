042b8a24b4ec2ad86b623fdb44af6d984801b4e2
Seyijesu <olalekeoluwaseyi@gmail.com>
2022-09-26 20:48:51 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+/**
+ * _memset - sets memory to arry
+ * @s: array to set
+ * @b: value to set it
+ * @n: number of times
+ * Return: char value of s
+ */
+char *_memset(char *s, char b, unsigned int n);
