355368ce3a2a9ff8d3615131ee5ef692017f17dc
Seyijesu <olalekeoluwaseyi@gmail.com>
2022-09-27 19:19:40 -0700

    double pointer

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -91,3 +91,14 @@ int _putchar(char c);
  *
  */
 void print_diagsums(int *a, int size);
+
+/**
+ * set_string -  sets the value of a pointer to a char
+ *
+ * @s: double pointer
+ *
+ * @to: pointer;
+ *
+ * Return: void;
+ */
+void set_string(char **s, char *to);
