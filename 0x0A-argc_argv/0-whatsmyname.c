a8829e856656ef71f97aea1c97eb3fd70e78ec03
Seyijesu <olalekeoluwaseyi@gmail.com>
2022-09-30 07:05:59 -0700

    print thr file name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - this print the name of the file
+ *
+ * @argc: this take the size of the arguments pass to the function
+ *
+ * @argv: a vector pointing to the string argument pass to the main
+ *
+ * Return: void
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", *argv[0]);
+
+	return (0);
+}
