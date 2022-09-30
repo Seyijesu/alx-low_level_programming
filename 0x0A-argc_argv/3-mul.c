dc2d4b93e7a1f7e888ed4c9e9017cb4e50eee699
Seyijesu <olalekeoluwaseyi@gmail.com>
2022-09-30 15:10:23 -0700

    multiplication of two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,26 +1,29 @@
 #include "main.h"
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  * main - this print the name of the file
  *
- * @first: the first number
+ * @argc: length of argv
  *
- * @second: second number
+ * @argv: contain the passed argument
  *
  * Return: 1 or 0
  */
-int main(int first, int second)
+int main(int argc, char *argv[])
 {
-	if (first && second)
+	if (argc == 3)
 	{
-		int result = first * second;
+		int result, first, second;
 
+		first = atoi(argv[1]);
+		second = atoi(argv[2]);
+		result = first * second;
 		printf("%d\n", result);
 	}
 	else
 	{
-		printf("Error");
+		printf("Error\n");
 		return (1);
 	}
 
