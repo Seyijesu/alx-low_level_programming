da55456d92c347e91f53a7c25a9c3b4091e037e8
Seyijesu <olalekeoluwaseyi@gmail.com>
2022-10-04 17:24:07 -0700

    Arry character

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,6 +15,7 @@
 char *create_array(unsigned int size, char c)
 {
 	char *myChar;
+	unsigned int i;
 
 	if (size == 0)
 		return (0);
@@ -23,6 +24,9 @@ char *create_array(unsigned int size, char c)
 	if (myChar == NULL)
 		return (NULL);
 
-	myChar[0] = *c;
+	for (i =0; i < size; i++)
+	{
+		myChar[i] = c;
+	}
 	return (myChar);
 }
