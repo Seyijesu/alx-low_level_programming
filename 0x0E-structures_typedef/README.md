1f55c1735a1503ff1b8f819530266c295c722807
Seyijesu <olalekeoluwaseyi@gmail.com>
2022-10-11 00:31:53 +0100

    dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This is directory for structure and typedef form alx task
---
+++ dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - this give name age and owner of dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+struct dog
+{
+    char * name;
+    float age;
+    char * owner;
+};
