3bc1143fe2b6cd8b4ae54d2dd556df5bd31c7688
Seyijesu <olalekeoluwaseyi@gmail.com>
2022-10-11 04:32:59 +0100

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,32 +17,35 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *dog;
 
 	dog = malloc(sizeof(dog_t));
-
+	if (dog == NULL)
+		return (NULL);
 	for (i = 0; name[i] != '\0'; i++)
 		lenName++;
-
 	for (j = 0; owner[j] != '\0'; j++)
 		lenOwner++;
-
 	dog->name = malloc((sizeof(dog->name) * lenName) + 1);
-	dog->owner = malloc((sizeof(dog->owner) * lenOwner) + 1);
-
-	if (dog->name == NULL || dog->owner == NULL)
+	if (dog->name == NULL)
+	{
+		free(dog);
 		return (NULL);
-
+	}
+	dog->owner = malloc((sizeof(dog->owner) * lenOwner) + 1);
+	if (dog->owner == NULL)
+	{
+		free(dog);
+		free(dog->name);
+		return (NULL);
+	}
 	for (i = 0; name[i] != '\0'; i++)
 	{
 		dog->name[i] = name[i];
 	}
 	dog->name[i] = '\0';
-
 	for (j = 0; owner[j] != '\0'; j++)
 	{
 		dog->owner[i] = owner[j];
 	}
 	dog->owner[j] = '\0';
-
 	dog->age = age;
-
 	return (dog);
 }
