9bd3c6313e12d7fece9c100232492951e8e5d364
Seyijesu <olalekeoluwaseyi@gmail.com>
2022-10-13 05:58:43 +0100

    SOP

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,38 +1,11 @@
-#ifndef 3_CALC_H
-#define 3_CALC_H
-
-/**
- * op_add - add two numbers
- * @a: first number
- * @b: second number
- * Return: sume of a and b
- */
+#ifndef _3_CALC_H
+#define _3_CALC_H
 int op_add(int a, int b);
-
-/**
- * op_sub - defference of two numbers
- * @a: the first number
- * @b: the second number
- * Return: defference between a and b
- */
 int op_sub(int a, int b);
-
-/**
- * op_mul - calculate product of a and b
- * @a: first number
- * @b: second number
- * Return: product of a and b
- */
 int op_mul(int a, int b);
-
-/**
- * op_div - division of two numbers
- * @a: numerator
- * @b: non-zero denorminator
- * Return: division of a by b
- */
 int op_div(int a, int b);
-
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
 /**
  * struct op - Struct op
  *
@@ -44,12 +17,4 @@ typedef struct op
 	char *op;
 	int (*f)(int a, int b);
 } op_t;
-
-/**
- * op_mod - modulus of two number
- * @a: first number
- * @b: the divisor
- * Return: the modulus of a by b
- */
-int op_mod(int a, int b);
 #endif
