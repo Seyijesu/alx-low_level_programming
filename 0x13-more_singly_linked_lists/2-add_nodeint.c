c2f8e97b12314e95a358d57132ecf0c0b3538399
Seyijesu <olalekeoluwaseyi@gmail.com>
2022-10-26 03:48:32 +0100

    more code committed

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -3,6 +3,22 @@
 #include "lsits.h"
 
 /**
- * add_nodeint - adding new user to beginning of struct
- * @head: pointer pointing to the structure
- * @n 
+ * add_nodeint - adds a node at the end
+ * @head: start of node
+ * @n: value to give new node
+ * Return: the linked list
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+
+	*head = new;
+
+	return (*head);
+}
